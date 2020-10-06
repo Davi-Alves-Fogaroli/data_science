e081dd86b965167bde8a80f6564b29c829b939db
Davi <davi.a.fogaroli@gmail.com>
2020-10-06 18:03:28 -0300

    correção de acentos

--- hello_world.sql
+++ hello_world.sql
@@ -2,17 +2,17 @@
 
 SET SERVEROUTPUT ON  /*Habilita o output*/
 
---bloco an�nimo : blocos onde se cria e poem procedimentos conforme as necessidades.
+--bloco anonimo : blocos onde se cria e poem procedimentos conforme as necessidades.
 
-DECLARE -- (bloco an�nimo opcional) nele se declara variaveis ou estruturas.
+DECLARE -- (bloco anonimo opcional) nele se declara variaveis ou estruturas.
 
     v_loucura_do_vercionamento VARCHAR2(50):='Olá mundo';
  
-BEGIN -- (bloco an�nimo obrigatorio) indica o inicio das instru��es/procedimentos PL/SQL.
+BEGIN -- (bloco anonimo obrigatorio) indica o inicio das instrucoes/procedimentos PL/SQL.
 
     DBMS_OUTPUT.PUT_LINE(v_loucura_do_vercionamento); /*DBMS_OUTPUToutput=biblioteca   .PUT_LINE=comando para o output  (nomedavariavel v_) */
     
---EXCEPTION (bloco an�nimo opcional) trabalha/cria as exess�es ( la�os, condi��es...) que acontencem entre os processos BEGIN e END. Tratamento de problemas.
+--EXCEPTION (bloco anonimo opcional) trabalha/cria as exessoes ( lacos, condicoes...) que acontencem entre os processos BEGIN e END. Tratamento de problemas.
 
-END; -- (bloco an�nimo obrigatorio) Encerramento do BEGIN.
+END; -- (bloco anonimo obrigatorio) Encerramento do BEGIN.
 /   
